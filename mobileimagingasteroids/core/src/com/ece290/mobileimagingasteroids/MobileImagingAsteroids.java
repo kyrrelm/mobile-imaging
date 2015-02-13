900a75e6829525e469b5d996d7f8e9c5d35686a9
ethanpreble <ethanpreble@gmail.com>
2015-02-13 14:40:47 -0800

    Adding base libgdx project
    Might need to remove some of the project files and might need to add the
    libgdx library

---
+++ mobileimagingasteroids/core/src/com/ece290/mobileimagingasteroids/MobileImagingAsteroids.java
@@ -0,0 +1,30 @@
+package com.ece290.mobileimagingasteroids;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MobileImagingAsteroids extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+    Texture img2;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+        img2 = new Texture("bg.png");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+        batch.draw(img2, 500,500);
+		batch.end();
+	}
+}
