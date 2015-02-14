a4352abd7e339c11219896a578134eb927054cc0
ethanpreble <ethanpreble@gmail.com>
2015-02-13 17:03:24 -0800

    Added base GameScreen with stub methods

---
+++ mobileimagingasteroids/core/src/com/ece290/mobileimagingasteroids/screen/GameScreen.java
@@ -0,0 +1,55 @@
+package com.ece290.mobileimagingasteroids.screen;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by ethan_000 on 2/13/2015.
+ */
+public class GameScreen implements Screen {
+
+    @Override
+    public void render(float delta) {
+        //TODO fix this class stub
+        // /runTime += delta;
+        //mWorld.update(delta, runTime);
+        //mRenderer.render(runTime);
+
+        //camera.update();
+    }
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+        //mRenderer.dispose();
+        //mWorld.dispose();
+        //game.setScreen(new GameOverScreen(game));
+    }
+
+    @Override
+    public void hide() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void show() {
+        //TODO
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // TODO Auto-generated method stub
+
+    }
+}
