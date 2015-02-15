ea98466ffa6ffd69d4a88d64fe26e717943c0373
ethanpreble <ethanpreble@gmail.com>
2015-02-15 13:04:18 -0800

    Separating the game out into screens

--- mobileimagingasteroids/core/src/com/ece290/mobileimagingasteroids/MobileImagingAsteroids.java
+++ mobileimagingasteroids/core/src/com/ece290/mobileimagingasteroids/MobileImagingAsteroids.java
@@ -6,25 +6,31 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MobileImagingAsteroids extends ApplicationAdapter {
+import com.badlogic.gdx.Game;
+import com.ece290.mobileimagingasteroids.screen.GameScreen;
+
+public class MobileImagingAsteroids extends Game {
 	SpriteBatch batch;
 	Texture img;
     Texture img2;
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
+        setScreen(new GameScreen(this));
+
+		/*batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-        img2 = new Texture("bg.png");
+        img2 = new Texture("bg.png");*/
+
 	}
 
-	@Override
+	/*@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		/*Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
         batch.draw(img2, 500,500);
 		batch.end();
-	}
+	}*/
 }
--- mobileimagingasteroids/core/src/com/ece290/mobileimagingasteroids/screen/GameScreen.java
+++ mobileimagingasteroids/core/src/com/ece290/mobileimagingasteroids/screen/GameScreen.java
@@ -1,24 +1,63 @@
 package com.ece290.mobileimagingasteroids.screen;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
  * Created by ethan_000 on 2/13/2015.
  */
 public class GameScreen implements Screen {
 
+    private Game game;
+    private OrthographicCamera camera;
+    private float runTime;
+
+    SpriteBatch batch;
+    Texture img;
+    Texture img2;
+
+
+
+    public GameScreen(Game game)
+    {
+        Gdx.app.log("GameScreen", "Attached");
+        this.game = game;
+
+
+        /*camera = new OrthographicCamera();
+        camera.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        //camera.setToOrtho(true, 1920, 1080);
+
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+        img2 = new Texture("bg.png");*/
+    }
     @Override
     public void render(float delta) {
-        //TODO fix this class stub
-        // /runTime += delta;
+        Gdx.app.log("GameScreen", "render");
+        runTime += delta;
         //mWorld.update(delta, runTime);
         //mRenderer.render(runTime);
-
+        Gdx.gl.glClearColor(1, .5f, .2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        /*Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.draw(img2, 500,500);
+        batch.end();
+*/
         //camera.update();
     }
 
     @Override
     public void dispose() {
+        Gdx.app.log("GameScreen", "dispose");
         // TODO Auto-generated method stub
         //mRenderer.dispose();
         //mWorld.dispose();
@@ -27,28 +66,37 @@ public class GameScreen implements Screen {
 
     @Override
     public void hide() {
+        Gdx.app.log("GameScreen", "hide");
         // TODO Auto-generated method stub
 
     }
 
     @Override
     public void resume() {
+        Gdx.app.log("GameScreen", "resume");
         // TODO Auto-generated method stub
     }
 
     @Override
     public void show() {
-        //TODO
+        Gdx.app.log("GameScreen", "show");
+        //Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+       /* batch.begin();
+        batch.draw(img, 0, 0);
+        batch.draw(img2, 500,500);
+        batch.end();*/
     }
 
     @Override
     public void pause() {
+        Gdx.app.log("GameScreen", "pause");
         // TODO Auto-generated method stub
 
     }
 
     @Override
     public void resize(int width, int height) {
+        Gdx.app.log("GameScreen", "resize");
         // TODO Auto-generated method stub
 
     }
