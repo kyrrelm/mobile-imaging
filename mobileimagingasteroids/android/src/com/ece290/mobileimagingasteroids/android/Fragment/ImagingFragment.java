2b237d4c7bb72a37cb797d2c4f1101a68aaff8dd
ethanpreble <ethanpreble@gmail.com>
2015-02-27 16:41:51 -0800

    Adding the opencv4android module to the project

--- mobileimagingasteroids/android/src/com/ece290/mobileimagingasteroids/android/Fragment/ImagingFragment.java
+++ mobileimagingasteroids/android/src/com/ece290/mobileimagingasteroids/android/Fragment/ImagingFragment.java
@@ -7,13 +7,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.ece290.mobileimagingasteroids.android.R;
-
 /**
  * Created by ethan_000 on 2/14/2015.
  */
 public class ImagingFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.imaging_fragment_layout, container, false);
+        //return inflater.inflate(R.layout.test, container, false);
+
     }
+
 }
