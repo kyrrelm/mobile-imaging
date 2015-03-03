ccb4d07aaa4c8f17f0ca4ae58816716a9cfc80cf
ingeboPuzen <oftedal.ingeborg@gmail.com>
2015-03-02 17:11:03 -0800

    imported bullet class

--- mobileimagingasteroids/core/src/com/ece290/mobileimagingasteroids/gameobject/Bullet.java
+++ mobileimagingasteroids/core/src/com/ece290/mobileimagingasteroids/gameobject/Bullet.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.math.Polygon;
  * Created by ingeborgoftedal on 02.03.15.
  */
 public class Bullet extends GameObject{
-    public Bullet(int width, int height, float positionX, float positionY, float velocityX, float velocityY){
+    protected Bullet(int width, int height, float positionX, float positionY, float velocityX, float velocityY){
         super(width, height, positionX, positionY, velocityX, velocityY);
 
     }
