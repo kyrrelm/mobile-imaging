d0d6392880457ead868ebdd959d6eb1d3a7e0deb
ingeboPuzen <oftedal.ingeborg@gmail.com>
2015-03-06 12:15:36 -0800

    Shot class

--- mobileimagingasteroids/core/src/com/ece290/mobileimagingasteroids/GameWorld.java
+++ mobileimagingasteroids/core/src/com/ece290/mobileimagingasteroids/GameWorld.java
@@ -1,24 +1,25 @@
 package com.ece290.mobileimagingasteroids;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.ece290.mobileimagingasteroids.gameobject.Asteroid;
+import com.ece290.mobileimagingasteroids.gameobject.Shot;
 import com.ece290.mobileimagingasteroids.gameobject.Ship;
 
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * Created by ethan_000 on 2/15/2015.
  */
 public class GameWorld {
-
     private Rectangle rect = new Rectangle(0, 0, 320, 240);
+
     private int mWidth, mHeight;
     private Ship mShip;
-    private List<Asteroid> asteroids;
+
+    private ArrayList<Asteroid> asteroidList;
+    private ArrayList<Shot> bulletList;
 
     private int lives;
     private int score;
@@ -29,47 +30,22 @@ public class GameWorld {
     {
         this.mWidth = width;
         this.mHeight = height;
-        asteroids = new ArrayList<Asteroid>();
-        asteroids.add(new Asteroid(200,200,50,50,40,40));
-
         mShip = new Ship(mWidth/15,mHeight/15, mWidth/2, mHeight/2);
-        //mShip.setAccelerationY(2);
-        //mShip.setAccelerationY(1);
+        mShip.setAccelerationY(2);
+        mShip.setAccelerationY(1);
     }
     public void update(float delta) {
-        //Gdx.app.log("GameWorld", "update");
+        Gdx.app.log("GameWorld", "update");
         rect.x += 4;
         if (rect.x > Gdx.graphics.getWidth())
             rect.x = 0;
-        mShip.update(delta);
-        mShip.setRotationUpdate(5);
-        //mShip.setX(mShip.getX()+4);
-        //mShip.setRotation(mShip.getRotation()+20);
+        mShip.setX(mShip.getX()+4);
         if (mShip.getX() > Gdx.graphics.getWidth())
             mShip.setX(0);
-        for(Asteroid a : asteroids)
-        {
-            if (a.getX() > Gdx.graphics.getWidth())
-                a.setX(0);
-            if (a.getY() > Gdx.graphics.getHeight())
-                a.setY(0);
-            a.setRotationUpdate(10);
-            a.update(delta);
-        }
-
-        //TODO also will need collision for shooting
-
-        for(Asteroid a : asteroids)
-        {
-            if(Intersector.overlapConvexPolygons(mShip.getPolygon(), a.getPolygon()));
-            {
-                //TODO game over
-            }
-        }
 
     }
-
-    public Rectangle getRect() {return rect;}
+    public Rectangle getRect() {
+        return rect;
+    }
     public Ship getShip(){return mShip;}
-    public List<Asteroid> getAsteroids(){return asteroids;}
 }
